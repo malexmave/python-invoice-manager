9223685363a7a3757edba90b97aa77552b0e57e5
Max Maass <max@velcommuta.de>
2014-02-13 16:21:29 +0100

    Added TODO markers to SQLite specs, reordered tests and added more

--- tests/data.py
+++ tests/data.py
@@ -4,22 +4,6 @@ import manage.company
 import manage.customer
 import manage.invoice
 
-class CustomerDataInteractions(unittest.TestCase):
-	def testAddCustomer(self):
-		self.failUnless(True)
-
-	def testGetCustomer(self):
-		self.failUnless(True)
-
-	def testEditCustomer(self):
-		self.failUnless(True)
-
-	def testRetireCustomer(self):
-		self.failUnless(True)
-
-	def testReanimateCustomer(self):
-		self.failUnless(True)
-
 class ArticleDataInteractions(unittest.TestCase):
 	def testAddArticle(self):
 		self.failUnless(True)
@@ -36,13 +20,53 @@ class ArticleDataInteractions(unittest.TestCase):
 	def testRelaunchArticle(self):
 		self.failUnless(True)
 
+
 class CompanyDataInteractions(unittest.TestCase):
-	def testEditCompany(self):
+	def testEditCompanyData(self):
 		self.failUnless(True)
+
+	def testGetCompanyData(self):
+		self.failUnless(True)
+
+
+class CustomerDataInteractions(unittest.TestCase):
+	def testAddCustomer(self):
+		self.failUnless(True)
+
+	def testGetCustomer(self):
+		self.failUnless(True)
+
+	def testEditCustomer(self):
+		self.failUnless(True)
+
+	def testRetireCustomer(self):
+		self.failUnless(True)
+
+	def testReanimateCustomer(self):
+		self.failUnless(True)
+
+
+class InvoiceDataInteractions(unittest.TestCase):
+	def testNewInvoice(self):
+		self.failUnless(True)
+
+	def testGetInvoice(self):
+		self.failUnless(True)
+
+	def testEditInvoice(self):
+		self.failUnless(True)
+
+	def testFinalizeInvoice(self):
+		self.failUnless(True)
+
+	def testFinalizedInvoiceNotMutable(self):
+		self.failUnless(True)
+
 
 def suite():
 	suite = unittest.TestSuite()
-	suite.addTest(unittest.makeSuite(CustomerDataInteractions, 'test'))
 	suite.addTest(unittest.makeSuite(ArticleDataInteractions, 'test'))
 	suite.addTest(unittest.makeSuite(CompanyDataInteractions, 'test'))
+	suite.addTest(unittest.makeSuite(CustomerDataInteractions, 'test'))
+	suite.addTest(unittest.makeSuite(InvoiceDataInteractions, 'test'))
 	return suite

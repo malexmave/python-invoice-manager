f471d63f5fd3d1b4a31e4daa1c021d7dc94e61fe
Max Maass <max@velcommuta.de>
2014-02-14 14:04:23 +0100

    Moved tests for DB interaction into their own module

---
+++ tests/testDBInteraction.py
@@ -0,0 +1,63 @@
+import copy
+import unittest
+
+
+"""
+class ArticleDataInteractions(unittest.TestCase):
+	def testAddArticle(self):
+		self.failUnless(True)
+
+	def testGetArticle(self):
+		self.failUnless(True)
+
+	def testEditArticle(self):
+		self.failUnless(True)
+
+	def testDiscontinueArticle(self):
+		self.failUnless(True)
+
+	def testRelaunchArticle(self):
+		self.failUnless(True)
+
+
+class CompanyDataInteractions(unittest.TestCase):
+	def testEditCompanyData(self):
+		self.failUnless(True)
+
+	def testGetCompanyData(self):
+		self.failUnless(True)
+
+
+class CustomerDataInteractions(unittest.TestCase):
+	def testAddCustomer(self):
+		self.failUnless(True)
+
+	def testGetCustomer(self):
+		self.failUnless(True)
+
+	def testEditCustomer(self):
+		self.failUnless(True)
+
+	def testRetireCustomer(self):
+		self.failUnless(True)
+
+	def testReanimateCustomer(self):
+		self.failUnless(True)
+
+
+class InvoiceDataInteractions(unittest.TestCase):
+	def testNewInvoice(self):
+		self.failUnless(True)
+
+	def testGetInvoice(self):
+		self.failUnless(True)
+
+	def testEditInvoice(self):
+		self.failUnless(True)
+
+	def testFinalizeInvoice(self):
+		self.failUnless(True)
+
+	def testFinalizedInvoiceNotMutable(self):
+		self.failUnless(True)
+"""
--- tests/testdatatypes.py
+++ tests/testdatatypes.py
@@ -3,69 +3,7 @@ import copy
 import unittest
 
 import data.datatype
-import manage.article
-import manage.company
-import manage.customer
-import manage.invoice
-"""
-class ArticleDataInteractions(unittest.TestCase):
-	def testAddArticle(self):
-		self.failUnless(True)
 
-	def testGetArticle(self):
-		self.failUnless(True)
-
-	def testEditArticle(self):
-		self.failUnless(True)
-
-	def testDiscontinueArticle(self):
-		self.failUnless(True)
-
-	def testRelaunchArticle(self):
-		self.failUnless(True)
-
-
-class CompanyDataInteractions(unittest.TestCase):
-	def testEditCompanyData(self):
-		self.failUnless(True)
-
-	def testGetCompanyData(self):
-		self.failUnless(True)
-
-
-class CustomerDataInteractions(unittest.TestCase):
-	def testAddCustomer(self):
-		self.failUnless(True)
-
-	def testGetCustomer(self):
-		self.failUnless(True)
-
-	def testEditCustomer(self):
-		self.failUnless(True)
-
-	def testRetireCustomer(self):
-		self.failUnless(True)
-
-	def testReanimateCustomer(self):
-		self.failUnless(True)
-
-
-class InvoiceDataInteractions(unittest.TestCase):
-	def testNewInvoice(self):
-		self.failUnless(True)
-
-	def testGetInvoice(self):
-		self.failUnless(True)
-
-	def testEditInvoice(self):
-		self.failUnless(True)
-
-	def testFinalizeInvoice(self):
-		self.failUnless(True)
-
-	def testFinalizedInvoiceNotMutable(self):
-		self.failUnless(True)
-"""
 class ArticleType(unittest.TestCase):
 	def setUp(self):
 		self.template = {"ID": 1, 
